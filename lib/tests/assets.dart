3ea91051451990fb1b3240df2fd5b9e51fdbd238
Vinícius M. Andreghetti <vinicius.andreghetti@gmail.com>
2024-07-14 00:41:13 -0300

    create testing static classes
    - classes that hold artificial (fake) objects for testing purposes.

---
+++ lib/tests/assets.dart
@@ -0,0 +1,110 @@
+import 'package:tractian_tree_view/models/asset.dart';
+import 'package:tractian_tree_view/tests/companies.dart';
+
+class Assets {
+  static var values = <String, List<Asset>>{};
+
+  static Map<String, List<Asset>> get() {
+    if (values.isEmpty) {
+      initValues();
+    }
+    return values;
+  }
+  
+  static List<Asset> getByCompany(String companyId) {
+    if (values.isEmpty) {
+      initValues();
+    }
+    return values[companyId] ?? [];
+  }
+
+  static void initValues() {
+    for (final company in Companies.values) {
+      values[company.id] = [
+        Asset(
+          id: "656a07bbf2d4a1001e2144c2",
+          locationId: "656a07b3f2d4a1001e2144bf",
+          name: "CONVEYOR BELT ASSEMBLY",
+          parentId: null,
+          sensorType: null,
+          status: null,
+        ),
+        Asset(
+          gatewayId: "QHI640",
+          id: "656734821f4664001f296973",
+          locationId: null,
+          name: "Fan - External",
+          parentId: null,
+          sensorId: "MTC052",
+          sensorType: "energy",
+          status: "operating",
+        ),
+        Asset(
+          id: "656734448eb037001e474a62",
+          locationId: "656733b1664c41001e91d9ed",
+          name: "Fan H12D",
+          parentId: null,
+          sensorType: null,
+          status: null,
+        ),
+        Asset(
+          gatewayId: "FRH546",
+          id: "656a07cdc50ec9001e84167b",
+          locationId: null,
+          name: "MOTOR RT COAL AF01",
+          parentId: "656a07c3f2d4a1001e2144c5",
+          sensorId: "FIJ309",
+          sensorType: "vibration",
+          status: "operating",
+        ),
+        Asset(
+          id: "656a07c3f2d4a1001e2144c5",
+          locationId: null,
+          name: "MOTOR TC01 COAL UNLOADING AF02",
+          parentId: "656a07bbf2d4a1001e2144c2",
+          sensorType: null,
+          status: null,
+        ),
+        Asset(
+          gatewayId: "QBK282",
+          id: "6567340c1f4664001f29622e",
+          locationId: null,
+          name: "Motor H12D- Stage 1",
+          parentId: "656734968eb037001e474d5a",
+          sensorId: "CFX848",
+          sensorType: "vibration",
+          status: "alert",
+        ),
+        Asset(
+          gatewayId: "VHS387",
+          id: "6567340c664c41001e91dceb",
+          locationId: null,
+          name: "Motor H12D-Stage 2",
+          parentId: "656734968eb037001e474d5a",
+          sensorId: "GYB119",
+          sensorType: "vibration",
+          status: "alert",
+        ),
+        Asset(
+          gatewayId: "VZO694",
+          id: "656733921f4664001f295e9b",
+          locationId: null,
+          name: "Motor H12D-Stage 3",
+          parentId: "656734968eb037001e474d5a",
+          sensorId: "SIF016",
+          sensorType: "vibration",
+          status: "alert",
+        ),
+        Asset(
+          id: "656734968eb037001e474d5a",
+          locationId: "656733b1664c41001e91d9ed",
+          name: "Motors H12D",
+          parentId: null,
+          sensorType: null,
+          status: null,
+        ),
+      ];
+    }
+  }
+
+}
---
+++ lib/tests/companies.dart
@@ -0,0 +1,9 @@
+import 'package:tractian_tree_view/models/company.dart';
+
+class Companies {
+  static const List<Company> values = [
+    Company(id: 'jaguarFakeId', name: 'Jaguar Unit [Test]'),
+    Company(id: 'tobiasFakeId', name: 'Tobias Unit [Test]'),
+    Company(id: 'apexFakeId', name: 'Apex Unit [Test]'),
+  ];
+}
