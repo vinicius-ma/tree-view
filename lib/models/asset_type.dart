e698e6ab503009b0ecd3855b53dd61ccd7fcc831
Vinícius M. Andreghetti <vinicius.andreghetti@gmail.com>
2024-07-16 23:32:34 -0300

    add icon to AssetType.unknown
    - AssetType.icon is required again
    - AssetType.unknown now has an icon so the app doesn't crash if an error
        occurs

--- lib/models/asset_type.dart
+++ lib/models/asset_type.dart
@@ -2,10 +2,10 @@ enum AssetType {
   location(name: "location", icon: "location"),
   asset(name: "asset", icon: "asset"),
   component(name: "component", icon: "component"),
-  unknown(name: "unknown");
+  unknown(name: "unknown", icon:"error");
 
   final String name;
   final String? icon;
 
-  const AssetType({required this.name, this.icon});
+  const AssetType({required this.name, required this.icon});
 }
